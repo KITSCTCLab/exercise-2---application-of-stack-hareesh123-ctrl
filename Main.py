2127da51d623bf73ba44e1b5601a5195549bf10e
URK21CS7001-HAREESH KUMAR <110516162+hareesh123-ctrl@users.noreply.github.com>
2022-08-16 20:59:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -70,3 +70,42 @@ class Evaluate:
       True if the expression is valid, else returns False.
     """
     for x in expression:
+        if x.isnumeric()==True:
+            return True
+        else:
+            if x=="+" or x=="-" or x=="" or x=="/" or x=="*":
+                return True
+            else:
+                return False
+   
+
+
+  def evaluate_postfix_expression(self, expression):
+    """
+    Evaluate the postfix expression
+    Arguments:
+      expression: A String which represents the the expression to be evaluated
+    Returns:
+      The result of evaluated postfix expression.
+    """
+    for i in expression:
+        if i.isnumeric():
+            self.push(int(i))
+        else:
+           a=self._pop()
+           b=self._pop()
+           operations_dictionary={"+":operator.add,"-":operator.sub,"*":operator.mul,"/":operator.floordiv,"^":operator.pow}
+           self.push(operations_dictionary[i](b,a))
+     
+    return self.stack[0]
+
+
+# Do not change the following code
+postfix_expression = input()  # Read postfix expression
+tokens = postfix_expression.split()
+evaluate = Evaluate(len(tokens))
+if evaluate.validate_postfix_expression(tokens):
+    print(evaluate.evaluate_postfix_expression(tokens))
+else:
+    print('Invalid postfix expression')
+
